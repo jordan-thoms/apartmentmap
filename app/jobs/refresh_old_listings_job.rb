84e2ab30df434b4465da7f20edb5dea7381671f7
Jordan Thoms <jordan@thoms.nz>
2018-03-03 18:44:06 +1300

    Fix query

--- app/jobs/refresh_old_listings_job.rb
+++ app/jobs/refresh_old_listings_job.rb
@@ -2,7 +2,7 @@ class RefreshOldListingsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    Listing.where(invalid_at: nil).where(expired_at: nil).where('updated_at < ?', 3.days.ago).find_each do |listing|
+    Listing.where(invalid_at: nil).where(expires_at: nil).where('updated_at < ?', 3.days.ago).find_each do |listing|
       ScrapeTrademeListingPageJob.set(wait_until: rand(50).minutes.from_now).perform_later listing.page_url
     end
   end
